8b393215363b96de72f007827cebfdee9004d412
zhangchenchen <zhangchenchen@itcast.cn>
2020-04-23 20:33:12 +0800

    将java工程添加到版本库

---
+++ prjuserview/src/main/java/com/view/main/PrjuserviewApplication.java
@@ -0,0 +1,13 @@
+package com.view.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjuserviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjuserviewApplication.class, args);
+    }
+
+}
---
+++ prjuserview/src/test/java/com/view/main/PrjuserviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.view.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjuserviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
